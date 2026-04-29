dc3de88b5e7c7365ca8675e7c50fc425ecb4252a
xjasonx <xjason.rogersx@gmail.com>
2026-04-29 22:58:29 +0100

    more cleanup

--- include/f1x/openauto/autoapp/UI/MainWindow.hpp
+++ include/f1x/openauto/autoapp/UI/MainWindow.hpp
@@ -18,131 +18,122 @@
 
 #pragma once
 
-#include <memory>
-#include <QMainWindow>
-#include <f1x/openauto/autoapp/Configuration/IConfiguration.hpp>
-
+#include <QBluetoothLocalDevice>
 #include <QFileSystemWatcher>
 #include <QKeyEvent>
+#include <QMainWindow>
+#include <f1x/openauto/autoapp/Configuration/IConfiguration.hpp>
+#include <memory>
+// #include <QtBluetooth>
 
-#include <QBluetoothLocalDevice>
-//#include <QtBluetooth>
-
-namespace Ui
-{
+namespace Ui {
 class MainWindow;
 }
 
-namespace f1x
-{
-namespace openauto
-{
-namespace autoapp
-{
-namespace ui
-{
+namespace f1x {
+namespace openauto {
+namespace autoapp {
+namespace ui {
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-public:
-    explicit MainWindow(configuration::IConfiguration::Pointer configuration, QWidget *parent = nullptr);
-    ~MainWindow() override;
-    QFileSystemWatcher* watcher_tmp; 
+class MainWindow : public QMainWindow {
+  Q_OBJECT
+ public:
+  explicit MainWindow(configuration::IConfiguration::Pointer configuration,
+                      QWidget* parent = nullptr);
+  ~MainWindow() override;
+  QFileSystemWatcher* watcher_tmp;
 
-signals:
-    void exit();
-    void reboot();
-    void openSettings();
-    void toggleCursor();
-    void TriggerScriptDay();
-    void TriggerScriptNight();
-    void openConnectDialog();
-    void openWifiDialog();
-    void openUpdateDialog();
-    void showAlphaSlider();
-    void TriggerAppStart();
-    void TriggerAppStop();
-    void CloseAllDialogs();
+ signals:
+  void exit();
+  void reboot();
+  void openSettings();
+  void toggleCursor();
+  void TriggerScriptDay();
+  void TriggerScriptNight();
+  void openConnectDialog();
+  void openWifiDialog();
+  void openUpdateDialog();
+  void showAlphaSlider();
+  void TriggerAppStart();
+  void TriggerAppStop();
+  void CloseAllDialogs();
 
-private slots:
-    void updateAlpha();
+ private slots:
+  void updateAlpha();
 
-private slots:
- 
-    void tmpChanged();
-    void setRetryUSBConnect();
-    void resetRetryUSBMessage();
-    void updateNetworkInfo();
-    bool check_file_exist(const char *filename);
-    void hostModeStateChanged(QBluetoothLocalDevice::HostMode);
+ private slots:
 
-private:
-    Ui::MainWindow* ui_;
-    configuration::IConfiguration::Pointer configuration_;
+  void tmpChanged();
+  void setRetryUSBConnect();
+  void resetRetryUSBMessage();
+  void updateNetworkInfo();
+  bool check_file_exist(const char* filename);
+  void hostModeStateChanged(QBluetoothLocalDevice::HostMode);
 
-    int alpha_current_str;
-    QString bversion;
-    QString bdate;
+ private:
+  Ui::MainWindow* ui_;
+  configuration::IConfiguration::Pointer configuration_;
 
-    char wifiButtonFile[32] = "/etc/button_wifi_visible";
-    char debugModeFile[32] = "/tmp/usb_debug_mode";
-    char lsFile[32] = "/etc/cs_lightsensor";
+  int alpha_current_str;
+  QString bversion;
+  QString bdate;
 
-    char custom_button_file_c1[26] = "/boot/crankshaft/button_1";
-    char custom_button_file_c2[26] = "/boot/crankshaft/button_2";
-    char custom_button_file_c3[26] = "/boot/crankshaft/button_3";
-    char custom_button_file_c4[26] = "/boot/crankshaft/button_4";
-    char custom_button_file_c5[26] = "/boot/crankshaft/button_5";
-    char custom_button_file_c6[26] = "/boot/crankshaft/button_6";
+  char wifiButtonFile[32] = "/etc/button_wifi_visible";
+  char debugModeFile[32] = "/tmp/usb_debug_mode";
+  char lsFile[32] = "/etc/cs_lightsensor";
 
-    QString custom_button_command_c1;
-    QString custom_button_command_c2;
-    QString custom_button_command_c3;
-    QString custom_button_command_c4;
-    QString custom_button_command_c5;
-    QString custom_button_command_c6;
+  char custom_button_file_c1[26] = "/boot/crankshaft/button_1";
+  char custom_button_file_c2[26] = "/boot/crankshaft/button_2";
+  char custom_button_file_c3[26] = "/boot/crankshaft/button_3";
+  char custom_button_file_c4[26] = "/boot/crankshaft/button_4";
+  char custom_button_file_c5[26] = "/boot/crankshaft/button_5";
+  char custom_button_file_c6[26] = "/boot/crankshaft/button_6";
 
-    QString custom_button_color_c1 = "186,189,192";
-    QString custom_button_color_c2 = "186,189,192";
-    QString custom_button_color_c3 = "186,189,192";
-    QString custom_button_color_c4 = "186,189,192";
-    QString custom_button_color_c5 = "186,189,192";
-    QString custom_button_color_c6 = "186,189,192";
+  QString custom_button_command_c1;
+  QString custom_button_command_c2;
+  QString custom_button_command_c3;
+  QString custom_button_command_c4;
+  QString custom_button_command_c5;
+  QString custom_button_command_c6;
 
-    bool wifiButtonForce = false;
+  QString custom_button_color_c1 = "186,189,192";
+  QString custom_button_color_c2 = "186,189,192";
+  QString custom_button_color_c3 = "186,189,192";
+  QString custom_button_color_c4 = "186,189,192";
+  QString custom_button_color_c5 = "186,189,192";
+  QString custom_button_color_c6 = "186,189,192";
 
+  bool wifiButtonForce = false;
 
-    bool exitMenuVisible = false;
+  bool exitMenuVisible = false;
 
-    bool rearCamEnabled = false;
-    bool rearCamVisible = false;
+  bool rearCamEnabled = false;
+  bool rearCamVisible = false;
 
-    bool dashCamRecording = false;
-    bool systemDebugmode = false;
+  bool dashCamRecording = false;
+  bool systemDebugmode = false;
 
-    bool bluetoothEnabled = false;
+  bool bluetoothEnabled = false;
 
-    bool toggleMute = false;
+  bool toggleMute = false;
 
-    bool hotspotActive = false;
-    bool background_set = false;
+  bool hotspotActive = false;
+  bool background_set = false;
 
-    bool lightsensor = false;
+  bool lightsensor = false;
 
-    bool csmtupdate = false;
-    bool udevupdate = false;
-    bool openautoupdate = false;
-    bool systemupdate = false;
+  bool csmtupdate = false;
+  bool udevupdate = false;
+  bool openautoupdate = false;
+  bool systemupdate = false;
 
-    QBluetoothLocalDevice *localDevice;
+  QBluetoothLocalDevice* localDevice;
 
-protected:
-    void keyPressEvent(QKeyEvent *event);
-
+ protected:
+  void keyPressEvent(QKeyEvent* event);
 };
 
-}
-}
-}
-}
+}  // namespace ui
+}  // namespace autoapp
+}  // namespace openauto
+}  // namespace f1x
--- src/autoapp/UI/MainWindow.cpp
+++ src/autoapp/UI/MainWindow.cpp
@@ -16,265 +16,257 @@
 *  along with openauto. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <QApplication>
-#include <f1x/openauto/autoapp/UI/MainWindow.hpp>
-#include <QFileInfo>
-#include "ui_mainwindow.h"
-#include <QTimer>
-#include <QDateTime>
-#include <QMessageBox>
-#include <QTextStream>
-#include <QFontDatabase>
-#include <QFont>
-#include <QScreen>
-#include <QRect>
-#include <QVideoWidget>
-#include <QNetworkInterface>
-#include <QStandardItemModel>
-#include <QPushButton>
-#include <iostream>
-#include <fstream>
-#include <cstdio>
 #include <unistd.h>
+
+#include <QApplication>
+#include <QDateTime>
+#include <QFileInfo>
+#include <QFont>
+#include <QFontDatabase>
+#include <QMessageBox>
+#include <QNetworkInterface>
+#include <QPushButton>
+#include <QRect>
+#include <QScreen>
+#include <QStandardItemModel>
+#include <QTextStream>
+#include <QTimer>
+#include <QVideoWidget>
+#include <cstdio>
 #include <f1x/openauto/Common/Log.hpp>
+#include <f1x/openauto/autoapp/UI/MainWindow.hpp>
+#include <fstream>
+#include <iostream>
 
-namespace f1x
-{
-namespace openauto
-{
-namespace autoapp
-{
-namespace ui
-{
+#include "ui_mainwindow.h"
 
-MainWindow::MainWindow(configuration::IConfiguration::Pointer configuration, QWidget *parent)
-    : QMainWindow(parent)
-    , ui_(new Ui::MainWindow)
-    , localDevice(new QBluetoothLocalDevice)
-{
-    // Set background image for the main window, fallback to black if missing.
-    const QString backgroundImage = "/home/pi/openauto/images/1777450215290.png";
-    if (QFileInfo::exists(backgroundImage)) {
-        this->setStyleSheet(QString(
-            "QMainWindow {"
-            "background-color: rgb(0,0,0);"
-            "background-image: url(%1);"
-            "background-position: center;"
-            "background-repeat: no-repeat;"
-            "}"
-        ).arg(backgroundImage));
+namespace f1x {
+namespace openauto {
+namespace autoapp {
+namespace ui {
+
+MainWindow::MainWindow(configuration::IConfiguration::Pointer configuration,
+                       QWidget* parent)
+    : QMainWindow(parent),
+      ui_(new Ui::MainWindow),
+      localDevice(new QBluetoothLocalDevice) {
+  // Set background image for the main window, fallback to black if missing.
+  const QString backgroundImage = "/home/pi/openauto/images/1777450215290.png";
+  if (QFileInfo::exists(backgroundImage)) {
+    this->setStyleSheet(QString("QMainWindow {"
+                                "background-color: rgb(0,0,0);"
+                                "background-image: url(%1);"
+                                "background-position: center;"
+                                "background-repeat: no-repeat;"
+                                "}")
+                            .arg(backgroundImage));
+  } else {
+    this->setStyleSheet("QMainWindow {background-color: rgb(0,0,0);}");
+  }
+
+  // Set default font and size
+  int id = QFontDatabase::addApplicationFont(":/Roboto-Regular.ttf");
+  QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+  QFont _font(family, 11);
+  qApp->setFont(_font);
+
+  this->configuration_ = configuration;
+
+  // trigger files
+
+  this->wifiButtonForce = check_file_exist(this->wifiButtonFile);
+  this->systemDebugmode = check_file_exist(this->debugModeFile);
+  this->lightsensor = check_file_exist(this->lsFile);
+
+  ui_->setupUi(this);
+
+  // Configure window attributes to prevent ghosting
+  this->setAttribute(Qt::WA_OpaquePaintEvent, true);
+  this->setAttribute(Qt::WA_NoSystemBackground, false);
+  this->setAutoFillBackground(true);
+
+  ui_->btDevice->hide();
+
+  // check if a device is connected via bluetooth
+  if (std::ifstream("/tmp/btdevice")) {
+    if (ui_->btDevice->isVisible() == false ||
+        ui_->btDevice->text().simplified() == "") {
+      QString btdevicename = configuration_->readFileContent("/tmp/btdevice");
+      ui_->btDevice->setText(btdevicename);
+      ui_->btDevice->show();
+    }
+  } else {
+    if (ui_->btDevice->isVisible() == true) {
+      ui_->btDevice->hide();
+    }
+  }
+
+  // hide wifi if not forced
+  if (!this->wifiButtonForce &&
+      !std::ifstream("/tmp/mobile_hotspot_detected")) {
+    ui_->AAWIFIWidget->hide();
+    ui_->AAWIFIWidget2->hide();
+  } else {
+    ui_->AAUSBWidget->hide();
+    ui_->AAUSBWidget2->hide();
+  }
+
+  // init alpha values
+  MainWindow::updateAlpha();
+
+  watcher_tmp = new QFileSystemWatcher(this);
+  watcher_tmp->addPath("/tmp");
+  connect(watcher_tmp, &QFileSystemWatcher::directoryChanged, this,
+          &MainWindow::tmpChanged);
+
+  // Experimental test code
+  localDevice = new QBluetoothLocalDevice(this);
+
+  connect(localDevice,
+          SIGNAL(hostModeStateChanged(QBluetoothLocalDevice::HostMode)), this,
+          SLOT(hostModeStateChanged(QBluetoothLocalDevice::HostMode)));
+
+  // Remove all push buttons from the main window UI.
+  for (auto* button : this->findChildren<QPushButton*>()) {
+    button->hide();
+    button->setEnabled(false);
+  }
+
+  hostModeStateChanged(localDevice->hostMode());
+  updateNetworkInfo();
+}
+
+MainWindow::~MainWindow() { delete ui_; }
+
+}  // namespace ui
+}  // namespace autoapp
+}  // namespace openauto
+}  // namespace f1x
+
+void f1x::openauto::autoapp::ui::MainWindow::hostModeStateChanged(
+    QBluetoothLocalDevice::HostMode mode) {
+  if (mode != QBluetoothLocalDevice::HostPoweredOff) {
+    this->bluetoothEnabled = true;
+    if (std::ifstream("/tmp/bluetooth_pairable")) {
+      ui_->labelBluetoothPairable->show();
     } else {
-        this->setStyleSheet("QMainWindow {background-color: rgb(0,0,0);}");
+      ui_->labelBluetoothPairable->hide();
     }
+  } else {
+    this->bluetoothEnabled = false;
+    ui_->labelBluetoothPairable->hide();
+  }
+}
 
-    // Set default font and size
-    int id = QFontDatabase::addApplicationFont(":/Roboto-Regular.ttf");
-    QString family = QFontDatabase::applicationFontFamilies(id).at(0);
-    QFont _font(family, 11);
-    qApp->setFont(_font);
+void f1x::openauto::autoapp::ui::MainWindow::updateNetworkInfo() {
+  QNetworkInterface wlan0if = QNetworkInterface::interfaceFromName("wlan0");
+  if (wlan0if.flags().testFlag(QNetworkInterface::IsUp)) {
+    QList<QNetworkAddressEntry> entrieswlan0 = wlan0if.addressEntries();
+    if (!entrieswlan0.isEmpty()) {
+      QNetworkAddressEntry wlan0 = entrieswlan0.first();
+      // qDebug() << "wlan0: " << wlan0.ip();
+      ui_->value_ip->setText(wlan0.ip().toString().simplified());
+      ui_->value_mask->setText(wlan0.netmask().toString().simplified());
+      if (std::ifstream("/tmp/hotspot_active")) {
+        ui_->value_ssid->setText(configuration_->getParamFromFile(
+            "/etc/hostapd/hostapd.conf", "ssid"));
+      } else {
+        ui_->value_ssid->setText(
+            configuration_->readFileContent("/tmp/wifi_ssid"));
+      }
+      ui_->value_gw->setText(
+          configuration_->readFileContent("/tmp/gateway_wlan0"));
+    }
+  } else {
+    // qDebug() << "wlan0: down";
+    ui_->value_ip->setText("");
+    ui_->value_mask->setText("");
+    ui_->value_gw->setText("");
+    ui_->value_ssid->setText("wlan0: down");
+  }
+}
 
-    this->configuration_ = configuration;
+void f1x::openauto::autoapp::ui::MainWindow::updateAlpha() {
+  int value = configuration_->getAlphaTrans();
+  this->alpha_current_str = value;
+}
 
-    // trigger files
-   
-    this->wifiButtonForce = check_file_exist(this->wifiButtonFile);
-    this->systemDebugmode = check_file_exist(this->debugModeFile);
-    this->lightsensor = check_file_exist(this->lsFile);
-   
+void f1x::openauto::autoapp::ui::MainWindow::setRetryUSBConnect() {
+  ui_->SysinfoTopLeft->setText("Trying USB connect ...");
+  ui_->SysinfoTopLeft->show();
 
+  QTimer::singleShot(10000, this, SLOT(resetRetryUSBMessage()));
+}
 
-    ui_->setupUi(this);
-    
-    // Configure window attributes to prevent ghosting
-    this->setAttribute(Qt::WA_OpaquePaintEvent, true);
-    this->setAttribute(Qt::WA_NoSystemBackground, false);
-    this->setAutoFillBackground(true);
+void f1x::openauto::autoapp::ui::MainWindow::resetRetryUSBMessage() {
+  ui_->SysinfoTopLeft->setText("");
+  ui_->SysinfoTopLeft->hide();
+}
 
-
-
-    ui_->btDevice->hide();
-
-    // check if a device is connected via bluetooth
-    if (std::ifstream("/tmp/btdevice")) {
-        if (ui_->btDevice->isVisible() == false || ui_->btDevice->text().simplified() == "") {
-            QString btdevicename = configuration_->readFileContent("/tmp/btdevice");
-            ui_->btDevice->setText(btdevicename);
-            ui_->btDevice->show();
-        }
+bool f1x::openauto::autoapp::ui::MainWindow::check_file_exist(
+    const char* fileName) {
+  std::ifstream ifile(fileName, std::ios::in);
+  // file not ok - checking if symlink
+  if (!ifile.good()) {
+    QFileInfo linkFile = QString(fileName);
+    if (linkFile.isSymLink()) {
+      QFileInfo linkTarget(linkFile.symLinkTarget());
+      return linkTarget.exists();
     } else {
-        if (ui_->btDevice->isVisible() == true) {
-            ui_->btDevice->hide();
-        }
+      return ifile.good();
     }
-
-
-
-    // hide wifi if not forced
-    if (!this->wifiButtonForce && !std::ifstream("/tmp/mobile_hotspot_detected")) {
-        ui_->AAWIFIWidget->hide();
-        ui_->AAWIFIWidget2->hide();
-    } else {
-        ui_->AAUSBWidget->hide();
-        ui_->AAUSBWidget2->hide();
-    }
-
-    // init alpha values
-    MainWindow::updateAlpha();
-
-    watcher_tmp = new QFileSystemWatcher(this);
-    watcher_tmp->addPath("/tmp");
-    connect(watcher_tmp, &QFileSystemWatcher::directoryChanged, this, &MainWindow::tmpChanged);
-
-    // Experimental test code
-    localDevice = new QBluetoothLocalDevice(this);
-
-    connect(localDevice, SIGNAL(hostModeStateChanged(QBluetoothLocalDevice::HostMode)),
-            this, SLOT(hostModeStateChanged(QBluetoothLocalDevice::HostMode)));
-
-    // Remove all push buttons from the main window UI.
-    for (auto* button : this->findChildren<QPushButton*>())
-    {
-        button->hide();
-        button->setEnabled(false);
-    }
-
-    hostModeStateChanged(localDevice->hostMode());
-    updateNetworkInfo();
+  } else {
+    return ifile.good();
+  }
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui_;
+void f1x::openauto::autoapp::ui::MainWindow::keyPressEvent(QKeyEvent* event) {
+  if (event->key() == Qt::Key_Return) {
+    QApplication::postEvent(
+        QApplication::focusWidget(),
+        new QKeyEvent(QEvent::KeyPress, Qt::Key_Space, Qt::NoModifier));
+    QApplication::postEvent(
+        QApplication::focusWidget(),
+        new QKeyEvent(QEvent::KeyRelease, Qt::Key_Space, Qt::NoModifier));
+  }
+  if (event->key() == Qt::Key_1) {
+    QApplication::postEvent(
+        QApplication::focusWidget(),
+        new QKeyEvent(QEvent::KeyPress, Qt::Key_Tab, Qt::ShiftModifier));
+  }
+  if (event->key() == Qt::Key_2) {
+    QApplication::postEvent(
+        QApplication::focusWidget(),
+        new QKeyEvent(QEvent::KeyPress, Qt::Key_Tab, Qt::NoModifier));
+  }
+  if (event->key() == Qt::Key_Escape) {
+  }
 }
 
-}
-}
-}
-}
+void f1x::openauto::autoapp::ui::MainWindow::tmpChanged() {
+  OPENAUTO_LOG(info) << "tmpChanged";
 
-void f1x::openauto::autoapp::ui::MainWindow::hostModeStateChanged(QBluetoothLocalDevice::HostMode mode)
-{
-    if (mode != QBluetoothLocalDevice::HostPoweredOff) {
-        this->bluetoothEnabled = true;
-        if (std::ifstream("/tmp/bluetooth_pairable")) {
-            ui_->labelBluetoothPairable->show();
-        } else {
-            ui_->labelBluetoothPairable->hide();
-        }
-    } else {
-        this->bluetoothEnabled = false;
-        ui_->labelBluetoothPairable->hide();
+  try {
+    if (std::ifstream("/tmp/entityexit")) {
+      MainWindow::TriggerAppStop();
+      std::remove("/tmp/entityexit");
     }
-}
+  } catch (...) {
+    OPENAUTO_LOG(error) << "[OpenAuto] Error in entityexit";
+  }
 
-void f1x::openauto::autoapp::ui::MainWindow::updateNetworkInfo()
-{
-    QNetworkInterface wlan0if = QNetworkInterface::interfaceFromName("wlan0");
-    if (wlan0if.flags().testFlag(QNetworkInterface::IsUp)) {
-        QList<QNetworkAddressEntry> entrieswlan0 = wlan0if.addressEntries();
-        if (!entrieswlan0.isEmpty()) {
-            QNetworkAddressEntry wlan0 = entrieswlan0.first();
-            //qDebug() << "wlan0: " << wlan0.ip();
-            ui_->value_ip->setText(wlan0.ip().toString().simplified());
-            ui_->value_mask->setText(wlan0.netmask().toString().simplified());
-            if (std::ifstream("/tmp/hotspot_active")) {
-                ui_->value_ssid->setText(configuration_->getParamFromFile("/etc/hostapd/hostapd.conf","ssid"));
-            } else {
-                ui_->value_ssid->setText(configuration_->readFileContent("/tmp/wifi_ssid"));
-            }
-            ui_->value_gw->setText(configuration_->readFileContent("/tmp/gateway_wlan0"));
-        }
-    } else {
-        //qDebug() << "wlan0: down";
-        ui_->value_ip->setText("");
-        ui_->value_mask->setText("");
-        ui_->value_gw->setText("");
-        ui_->value_ssid->setText("wlan0: down");
+  if (std::ifstream("/tmp/blankscreen")) {
+    if (ui_->centralWidget->isVisible()) {
+      CloseAllDialogs();
+      ui_->centralWidget->hide();
     }
-}
+  } else if (!ui_->centralWidget->isVisible()) {
+    ui_->centralWidget->show();
+  }
 
-void f1x::openauto::autoapp::ui::MainWindow::updateAlpha()
-{
-    int value = configuration_->getAlphaTrans();
-    this->alpha_current_str = value;
-}
+  if (std::ifstream("/tmp/external_exit")) {
+    f1x::openauto::autoapp::ui::MainWindow::MainWindow::exit();
+  }
 
-
-
-
-void f1x::openauto::autoapp::ui::MainWindow::setRetryUSBConnect()
-{
-    ui_->SysinfoTopLeft->setText("Trying USB connect ...");
-    ui_->SysinfoTopLeft->show();
-
-    QTimer::singleShot(10000, this, SLOT(resetRetryUSBMessage()));
-}
-
-void f1x::openauto::autoapp::ui::MainWindow::resetRetryUSBMessage()
-{
-    ui_->SysinfoTopLeft->setText("");
-    ui_->SysinfoTopLeft->hide();
-}
-
-bool f1x::openauto::autoapp::ui::MainWindow::check_file_exist(const char *fileName)
-{
-    std::ifstream ifile(fileName, std::ios::in);
-    // file not ok - checking if symlink
-    if (!ifile.good()) {
-        QFileInfo linkFile = QString(fileName);
-        if (linkFile.isSymLink()) {
-            QFileInfo linkTarget(linkFile.symLinkTarget());
-            return linkTarget.exists();
-        } else {
-            return ifile.good();
-        }
-    } else {
-        return ifile.good();
-    }
-}
-
-void f1x::openauto::autoapp::ui::MainWindow::keyPressEvent(QKeyEvent *event)
-{
-    if (event->key() == Qt::Key_Return) {
-        QApplication::postEvent (QApplication::focusWidget(), new QKeyEvent ( QEvent::KeyPress, Qt::Key_Space, Qt::NoModifier));
-        QApplication::postEvent (QApplication::focusWidget(), new QKeyEvent ( QEvent::KeyRelease, Qt::Key_Space, Qt::NoModifier));
-    }
-    if (event->key() == Qt::Key_1) {
-        QApplication::postEvent (QApplication::focusWidget(), new QKeyEvent ( QEvent::KeyPress, Qt::Key_Tab, Qt::ShiftModifier));
-    }
-    if (event->key() == Qt::Key_2) {
-        QApplication::postEvent (QApplication::focusWidget(), new QKeyEvent ( QEvent::KeyPress, Qt::Key_Tab, Qt::NoModifier));
-    }
-    if(event->key() == Qt::Key_Escape)
-    {
-    }
-}
-
-void f1x::openauto::autoapp::ui::MainWindow::tmpChanged()
-{
-    OPENAUTO_LOG(info) << "tmpChanged";
-
-    try {
-        if (std::ifstream("/tmp/entityexit")) {
-            MainWindow::TriggerAppStop();
-            std::remove("/tmp/entityexit");
-        }
-    } catch (...) {
-        OPENAUTO_LOG(error) << "[OpenAuto] Error in entityexit";
-    }
-
-    if (std::ifstream("/tmp/blankscreen")) {
-        if (ui_->centralWidget->isVisible()) {
-            CloseAllDialogs();
-            ui_->centralWidget->hide();
-        }
-    } else if (!ui_->centralWidget->isVisible()) {
-        ui_->centralWidget->show();
-    }
-
-    if (std::ifstream("/tmp/external_exit")) {
-        f1x::openauto::autoapp::ui::MainWindow::MainWindow::exit();
-    }
-
-    updateNetworkInfo();
+  updateNetworkInfo();
 }
